58d9cb20a2259e4a4d3924115255fc789c7dbdb7
Graham Ward <grahamjohnward@gmail.com>
2023-01-15 15:45:11 +0000

    Add --heap-size argument to main

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include "lisp.h"
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(int argc, char **argv)
 {
-    init_interpreter(65536);
+    int i = 1;
+    size_t heap_size = 65536; /* Default */
+    if (strcmp(argv[i], "--heap-size") == 0) {
+        i++;
+        char *endptr;
+        errno = 0;
+        heap_size = strtol(argv[i], &endptr, 10);
+        if (errno) {
+            perror("Heap size");
+            exit(1);
+        } else if (*endptr != '\0') {
+            printf("Bad heap size %s\n", argv[i]);
+            exit(1);
+        } else {
+            i++;
+        }
+    }
+    init_interpreter(heap_size);
     top_of_stack = (lisp_object_t *)get_rbp(1);
-    for (int i = 1; i < argc; i++) {
+    for (; i < argc; i++) {
         load_str(argv[i]);
     }
     free_interpreter();

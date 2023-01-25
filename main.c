1ffda114598893c6aba0300ad483dbd96a888129
Graham Ward <grahamjohnward@gmail.com>
2023-01-25 18:57:48 +0000

    Use getopt_long in main

--- main.c
+++ main.c
@@ -1,34 +1,59 @@
 #include "lisp.h"
 
 #include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv)
+struct interpreter_settings {
+    size_t heap_size;
+};
+
+static struct option options[] = {
+    { "heap-size", optional_argument, 0, 0 },
+    { 0, 0, 0, 0 }
+};
+
+static int parse_args(int argc, char **argv, struct interpreter_settings *settings)
 {
-    int i = 1;
-    size_t heap_size = 65536; /* Default */
-    if (strcmp(argv[i], "--heap-size") == 0) {
-        i++;
-        char *endptr;
-        errno = 0;
-        heap_size = strtol(argv[i], &endptr, 10);
-        if (errno) {
-            perror("Heap size");
-            exit(1);
-        } else if (*endptr != '\0') {
-            printf("Bad heap size %s\n", argv[i]);
-            exit(1);
-        } else {
-            i++;
+    settings->heap_size = 65536; /* default */
+    int c;
+    while (1) {
+        c = getopt_long_only(argc, argv, "", options, NULL);
+        printf("%d\n", c);
+        if (c == -1)
+            break;
+        switch (c) {
+        case 0:
+            if (optarg) {
+                char *endptr;
+                errno = 0;
+                settings->heap_size = strtol(optarg, &endptr, 10);
+                if (errno) {
+                    perror("Heap size");
+                    exit(1);
+                } else if (*endptr != '\0') {
+                    printf("Bad heap size %s\n", optarg);
+                    exit(1);
+                }
+            }
+            break;
+        default:
+            abort();
         }
     }
-    init_interpreter(heap_size);
+    return optind;
+}
+
+int main(int argc, char **argv)
+{
+    struct interpreter_settings settings;
+    int i = parse_args(argc, argv, &settings);
+    init_interpreter(settings.heap_size);
     top_of_stack = (lisp_object_t *)get_rbp(1);
-    for (; i < argc; i++) {
+    for (; i < argc; i++)
         load_str(argv[i]);
-    }
     free_interpreter();
     return 0;
 }

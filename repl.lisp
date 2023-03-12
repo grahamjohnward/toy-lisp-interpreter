ae9b558a938d215265f8c0f17e9bae8993b034a2
Graham Ward <grahamjohnward@gmail.com>
2023-03-12 19:32:15 +0000

    Minor REPL improvements

--- repl.lisp
+++ repl.lisp
@@ -1,13 +1,15 @@
 (condition-case eof
     (let (input)
       (tagbody
+	 (print "Welcome to Graham's Lisp")
        repl
 	 (princ "> ")
 	 (set 'input (read))
 	 (condition-case e
 	     (print (eval input))
+	   (runtime-error (print e))
 	   (unbound-variable (print e))
 	   (type-error (print e)))
 	 (go repl)))
   (end-of-file
-   (exit 0))))
+   (exit 0)))

d1f391c263fe2eeb1182fb3ed08f2a5ee94c1bcc
Graham Ward <grahamjohnward@gmail.com>
2025-05-25 06:57:15 +0100

    Remove lexical env lookup from C evaluator

--- lexical_scope.c
+++ lexical_scope.c
@@ -11,8 +11,6 @@ void lexical_context_init(struct lexical_context *ctxt)
 {
     ctxt->block_alist = NIL;
     ctxt->next_block_number = 0;
-    ctxt->bindings = NIL;
-    ctxt->n_bindings = 0;
 }
 
 lisp_object_t lexical_context_enter_block(struct lexical_context *ctxt, lisp_object_t block_name)
@@ -51,36 +49,4 @@ static lisp_object_t reverse1(lisp_object_t list, lisp_object_t aux)
 static lisp_object_t reverse(lisp_object_t list)
 {
     return reverse1(list, NIL);
-}
-
-void lexical_context_enter_scope(struct lexical_context *ctxt, lisp_object_t bindings)
-{
-    ctxt->bindings = cons(bindings, ctxt->bindings);
-    lisp_object_t len = length(bindings);
-    ctxt->n_bindings += len;
-    TRACE(ctxt->bindings);
-}
-
-void lexical_context_leave_scope(struct lexical_context *ctxt)
-{
-    ctxt->n_bindings -= length(car(ctxt->bindings));
-    ctxt->bindings = cdr(ctxt->bindings);
-}
-
-lisp_object_t lexical_context_lookup(struct lexical_context *ctxt, lisp_object_t symbol)
-{
-    lisp_object_t bindings = ctxt->bindings;
-    lisp_object_t i = ctxt->n_bindings;
-    while (bindings != NIL) {
-        lisp_object_t bindings_one_scope = car(bindings);
-        while (bindings_one_scope != NIL) {
-            if (eq(car(bindings_one_scope), symbol) != NIL) {
-                return i;
-            }
-            i -= 16;
-            bindings_one_scope = cdr(bindings_one_scope);
-        }
-        bindings = cdr(bindings);
-    }
-    return NIL;
 }
--- lexical_scope.h
+++ lexical_scope.h
@@ -3,8 +3,6 @@
 struct lexical_context {
     lisp_object_t block_alist;
     lisp_object_t next_block_number;
-    lisp_object_t bindings;
-    lisp_object_t n_bindings;
 };
 
 void lexical_context_init(struct lexical_context *ctxt);
@@ -12,9 +10,3 @@ void lexical_context_init(struct lexical_context *ctxt);
 lisp_object_t lexical_context_enter_block(struct lexical_context *ctxt, lisp_object_t block_name);
 
 void lexical_context_leave_block(struct lexical_context *ctxt, lisp_object_t block_name);
-
-void lexical_context_enter_scope(struct lexical_context *ctxt, lisp_object_t bindings);
-
-void lexical_context_leave_scope(struct lexical_context *ctxt);
-
-lisp_object_t lexical_context_lookup(struct lexical_context *ctxt, lisp_object_t sym);

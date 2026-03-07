1108d684bc9ca3e156a025c72b025f524d13123a
Graham Ward <grahamjohnward@gmail.com>
2026-03-07 07:20:14 +0000

    Scripts for bootstrapping the compiler

---
+++ bootstrap.lisp
@@ -0,0 +1,28 @@
+(let ((files-to-compile '(("lib.lisp" . "lib.compiled")
+                          ("batch-compile.lisp" . "batch-compile.compiled")
+                          ("repl.lisp" . "repl.compiled")
+			  ("compiler.lisp" . "compiler.compiled")
+			  ("vmboot.lisp" . "vmboot.compiled")
+                          ("eval.lisp" . "eval.compiled")
+			  ("bootstrap.lisp" . "bootstrap.compiled"))))
+  (tagbody
+   next-file
+     (if (eq files-to-compile nil)
+	 (go done))
+     (let ((file (car files-to-compile)))
+       (print file)
+       (let ((source-file (car file))
+	     (compiled-file (cdr file)))
+	 (let ((in-stream (open source-file :read))
+	       (out-stream (open compiled-file :write)))
+	   (condition-case eof
+	       (let (input)
+		 (tagbody
+		  loop
+		    (setq input (read1 in-stream))
+		    (write (compile4-toplevel input) out-stream)
+		    (go loop)))
+	     (end-of-file nil)))))
+     (setq files-to-compile (cdr files-to-compile))
+     (go next-file)
+   done))
---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+./main --heap-size=64m lib.lisp compiler.lisp bootstrap.lisp
+./main --heap-size=128m --use-vm --vmboot=vmboot.compiled lib.compiled compiler.compiled apply.compiled bootstrap.compiled

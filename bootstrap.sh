4930e6c0ede14a0507950dd060c1770aca4cb2be
Graham Ward <grahamjohnward@gmail.com>
2026-06-07 07:05:24 +0100

    Timing in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-./main --heap-size=64m lib.lisp compiler.lisp bootstrap.lisp
-./main --heap-size=128m --use-vm --vmboot=vmboot.compiled lib.compiled compiler.compiled apply.compiled bootstrap.compiled
+./main --heap-size=64m lib.lisp compiler.lisp bootstrap.lisp &&
+time ./main --heap-size=128m --use-vm --vmboot=vmboot.compiled lib.compiled compiler.compiled apply.compiled bootstrap.compiled

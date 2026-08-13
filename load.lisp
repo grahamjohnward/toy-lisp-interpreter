c4590b93ffe4ba1bc2fe1443067b8dc5284b2bb0
Graham Ward <grahamjohnward@gmail.com>
2026-08-13 14:18:55 +0100

    Use eval in load

--- load.lisp
+++ load.lisp
@@ -5,19 +5,6 @@
 	  (tagbody
 	   loop
 	     (setq input (read1 in-stream))
-	     (let ((code (compile-toplevel input)))
-               (let ((len (length code)))
-                 (let ((new-code (make-vector (two-arg-plus len 1)))
-		       (j 0))
-		   (tagbody
-		    loop2
-		      (set-svref new-code j (svref code j))
-		      (setq j (two-arg-plus j 1))
-		      (if (eq j len)
-			  (go last-bit))
-		      (go loop2)
-		    last-bit
-		      (set-svref new-code j 8))
-		   (print (funcall (%vm-make-simple-function #(nil 0) new-code)))))
-	       (go loop))))
+             (eval input)
+             (go loop)))
       (end-of-file t))))

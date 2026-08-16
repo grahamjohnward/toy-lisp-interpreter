6bd3c394dc1785030e4c45a44491e7c204ddd321
Graham Ward <grahamjohnward@gmail.com>
2026-08-16 16:18:54 +0100

    Put REPL code into a function called repl

--- repl.lisp
+++ repl.lisp
@@ -1,28 +1,31 @@
-(condition-case eof
-    (let (input result)
-      (tagbody
-	 (princ "Welcome to Graham's Lisp\n")
-       repl
-	 (princ "> ")
-	 (setq input (read))
-	 (condition-case e
-             (progn
-	       (setq *** **)
-	       (setq ** *)
-	       (setq * result)
-	       (setq result (eval input))
-	       (print result))
-	   (runtime-error (print e))
-	   (unbound-variable (print e))
-           (bad-function (print e))
-           (bad-arity (print e))
-	   (type-error
-            (progn
-              (princ "Type error: ")
-              (print e))))
-	 (setq +++ ++)
-	 (setq ++ +)
-	 (setq + input)
-	 (go repl)))
-  (end-of-file
-   (exit 0)))
+(defun repl ()
+  (condition-case eof
+      (let (input result)
+        (tagbody
+	   (princ "Welcome to Graham's Lisp\n")
+         repl
+	   (princ "> ")
+	   (setq input (read))
+	   (condition-case e
+               (progn
+	         (setq *** **)
+	         (setq ** *)
+	         (setq * result)
+	         (setq result (eval input))
+	         (print result))
+	     (runtime-error (print e))
+	     (unbound-variable (print e))
+             (bad-function (print e))
+             (bad-arity (print e))
+	     (type-error
+              (progn
+                (princ "Type error: ")
+                (print e))))
+	   (setq +++ ++)
+	   (setq ++ +)
+	   (setq + input)
+	   (go repl)))
+    (end-of-file
+     (exit 0))))
+
+(repl)

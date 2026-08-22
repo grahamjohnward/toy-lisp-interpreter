27058adb5235b94ad3a2f9a80c17e515d44d608a
Graham Ward <grahamjohnward@gmail.com>
2026-08-22 14:20:10 +0100

    Script to build image

---
+++ make-image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./main --heap-size=128m --use-vm --vmboot=vmboot.compiled --eval='(save-image "image")' lib.compiled compiler.compiled apply.compiled eval.compiled load.compiled raise.compiled repl.compiled
